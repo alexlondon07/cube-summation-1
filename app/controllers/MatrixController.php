fa741b7439d4c7e3382e399df6ec7a2b06f7ec2e
Nelson Medina <nelsonmedina6@gmail.com>
2016-12-25 18:42:02 -0400

    Built class to handle matrix operations

--- app/controllers/MatrixController.php
+++ app/controllers/MatrixController.php
@@ -2,29 +2,47 @@
 
 class MatrixController extends BaseController 
 {
-	private $matrix[][][];
-	private $N;
-	private $M;
+	private $n;
+	private $matrix;
 
-	public function init()
+	public function init($n)
 	{
-		for ($i=0; $i < $N; $i++) { 
-			for ($j=0; $j < $N ; $j++) { 
-				for ($k=0; $k < $N ; $k++) { 
-					$matrix[$i][$j][$k] = 0;
+		$this->n = $n;
+
+		for ($i=0; $i < $this->n; $i++) { 
+			
+			for ($j=0; $j < $this->n ; $j++) { 
+				
+				for ($k=0; $k <$this->n ; $k++) { 
+
+					$this->matrix[$i][$j][$k] = 0;
 				}
 			}
-		}	
+		}
 	}
 
 	public function update($i, $j, $k, $value)
 	{
-		$this->matrix[$i][$j][$k] = $value;
+		$this->matrix[$i - 1][$j - 1][$k - 1] = (int) $value;
 	}
 
-	public function getSum()
+	public function sum($x2, $y2, $z2, $x1, $y1, $z1)
 	{
+		
+		$sum = 0;
 
+		for ($i = $x2 - 1; $i >= $x1 - 1 ; $i --) { 
+			
+			for ($j = $y2 - 1; $j >= $y1 - 1 ; $j --) { 
+				
+				for ($k = $z2 - 1; $k >= $z1 - 1 ; $k --) { 
+
+					$sum += $this->matrix[$i][$j][$k];
+				}
+			}
+		}
+
+		return $sum;
 	}
 
 }
